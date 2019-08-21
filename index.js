9479029ded337f2545b746cf6a170e09c0dd24bb
Luke Honiss <luke.honiss@gmail.com>
2019-08-21 17:03:05 +1200

    Add ability to fetch videos from db

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongo = require('mongodb')
+const MongoClient = mongo.MongoClient;
 
 const app = express();
 
+let db;
+
 app.use(express.static('./'));
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.sendfile('./index.html');
 });
 
+app.get('/videos', (req, res) => {
+  // least recently played first, then by when they were uploaded
+  db.collection('clips').find({type: 'url'}).sort({lastPlayed: 1, uploadedAt: 1}).limit(1).toArray().then((output) => {
+    res.send(output[0]);
+  }).catch((error) => {
+    res.send(error)
+  });
+});
+
+app.post('/videos', (req, res) => {
+  const lastPlayed = new Date().toLocaleString().replace(/\//g, '-').replace(', ', '-');
+  db.collection('clips').updateOne({'_id': new mongo.ObjectID(req.body._id)}, {$set: {lastPlayed}}).then((output) => {
+    console.log(`Video updated: ${JSON.stringify(output.result)}`);
+    res.send(output.result)
+  }).catch((error) => {
+    res.send(error);
+  });
+});
+
 app.listen(3000, () => {
-  console.log('app listening on port 3000')
+  MongoClient.connect(process.env.DB_HOST, {useNewUrlParser: true, useUnifiedTopology: true}, ( err, client ) => {
+    if (err) {
+      throw new Error(err);
+    }
+    db = client.db('tf2frags');
+    console.log('app listening on port 3000');
+  });
 });
